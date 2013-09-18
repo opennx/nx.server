0536e1a47518cc46e03fd54897ec12ab3f40e152
Martas <martas@imm.cz>
2013-09-18 14:03:47 +0200

    Changes to be committed:    (use "git reset HEAD <file>..." to unstage)
    	new file:   nx/assets.py
    	new file:   nx/items.py
    	modified:   run.py

--- run.py
+++ run.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 import nx
 
-nx.config.user = "Hovnislav"
+import os, sys
 
+print os.getcwd()
+
+nx.config.user = "Hovnislav"
 nx.logging.info("Hello World")
