4a9705272963f3f84e5456b838b669bc4f05bf8a
Martastain <martas@imm.cz>
2013-12-31 17:40:49 +0100

    Fulltext fix

--- services/hive/hive_assets.py
+++ services/hive/hive_assets.py
@@ -2,20 +2,20 @@ from nx import *
 from nx.assets import *
 
 def hive_browse(auth_key, params):
-
     db = DB()
     conds = []
 
-    if "fulltext" in params:
-        cond = "id_asset IN (SELECT id_asset FROM nx_meta WHERE value LIKE '%%%s%%')" % db.sanit(params["fulltext"])
-        conds.append(cond)
+    if "fulltext" in params: # I'll definitely go to hell
+        conds.append("id_asset IN (SELECT id_asset FROM nx_meta WHERE %s)" % " AND ".join(["LOWER(value) LIKE '%%%s%%'"% db.sanit(elm.lower()) for elm in params["fulltext"].split()]))
 
     if conds:
-        qcon += " WHERE %s" % " AND ".join(conds)
+        qcon = " WHERE %s" % " AND ".join(conds)
     else:
         qcon = ""
 
     query = "SELECT id_asset FROM nx_assets%s" % qcon
+
+    print query
 
     asset_data = []
     db.query(query)
