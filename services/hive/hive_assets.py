fa5ac101322d00d726e24e5279299e278ef8cc92
root <root@nxcore>
2014-01-03 12:12:49 +0100

    Accent unaware search

--- services/hive/hive_assets.py
+++ services/hive/hive_assets.py
@@ -5,8 +5,11 @@ def hive_browse(auth_key, params):
     db = DB()
     conds = []
 
-    if "fulltext" in params: # I'll definitely go to hell
-        conds.append("id_asset IN (SELECT id_asset FROM nx_meta WHERE %s)" % " AND ".join(["LOWER(value) LIKE '%%%s%%'"% db.sanit(elm.lower()) for elm in params["fulltext"].split()]))
+    if params.get("fulltext", False): # I'll definitely go to hell
+        if config["db_driver"] == "postgres":
+            conds.append("id_asset IN (SELECT id_asset FROM nx_meta WHERE %s)" % " AND ".join(["unaccent(LOWER(value)) LIKE unaccent('%%%s%%')"% db.sanit(elm.lower()) for elm in params["fulltext"].split()]))
+        else:
+            conds.append("id_asset IN (SELECT id_asset FROM nx_meta WHERE %s)" % " AND ".join(["LOWER(value) LIKE '%%%s%%'"% db.sanit(elm.lower()) for elm in params["fulltext"].split()]))
 
     if conds:
         qcon = " WHERE %s" % " AND ".join(conds)
