837f7314a9cc070ebaf50b4bdfc8563d2095b9a8
martastain <martas@imm.cz>
2014-09-09 16:04:51 +0200

    NewsProvider update

--- services/news/__init__.py
+++ services/news/__init__.py
@@ -7,36 +7,7 @@ import uuid
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
 
 from nx import *
-from newsdb import NewsDB, create_db
-
-
-class Article():
-    def __init__(self, id_article=False, channel=False, db=False):
-        self.id_article = id_article
-        self._db = db
-        if id_article:
-            # load article from db
-            db.query()
-        else:
-            self.guid = str(uuid.uuid1())
-            self.content = {}
-            self.source = "default"
-
-    @property 
-    def db(self):
-        if not self._db:
-            self._db = NewsDB()
-        return self._db
-
-    @property 
-    def id(self):
-        return self.id_article
-
-    def save(self):
-        if not self.id:
-            db.query()
-
-
+from nx.objects import *
 
 
 class ControlHandler(BaseHTTPRequestHandler):
@@ -55,10 +26,9 @@ class ControlHandler(BaseHTTPRequestHandler):
     def _echo(self,istring):
         self.wfile.write(istring.encode("utf-8"))
 
-    def result(self, r):
-        response, data = r
-        self._do_headers(response=response)
-        self._echo(json.dumps([response, data]))
+    def result(self, data):
+        self._do_headers()
+        self._echo(json.dumps(data))
 
 
     def error(self,response):
@@ -66,27 +36,76 @@ class ControlHandler(BaseHTTPRequestHandler):
 
     def do_GET(self):
         service = self.server.service
-        self.result(service.stat())
+        article = service.get_article(1, ["google_domov", "google_svet"])
+        self.result(article.meta)
 
 
 
 class Service(ServicePrototype):
     def on_init(self):
         port = 42200
-        db_path = "news.db"
+        self.max_articles = 100
 
-        if not os.path.exists(db_path):
-            create_db(db_path)
-
-        self.news_db = NewsDB(db_path)
-
+        self.history = {}
         self.server = HTTPServer(('',port), ControlHandler)
         self.server.service = self
         thread.start_new_thread(self.server.serve_forever,())
 
 
-    def get_item(self, source, channel):
-        pass
+    def get_article(self, channel, groups):
+        db = DB()
+        db.query("""SELECT id_object FROM nx_assets WHERE id_folder = 6 AND mtime > {}
+            AND id_object IN (SELECT id_object FROM nx_meta WHERE tag='news_group' AND value IN ({}))
+            AND id_object IN (SELECT id_object FROM nx_meta WHERE tag='qc/state' AND value='4')
+            """.format(time.time() - 86400,   ", ".join(["'{}'".format(group) for group in groups])))
+        
+        articles = [r[0] for r in db.fetchall()]
+
+        if channel not in self.history:
+            self.history[channel] = {}
+
+        id_asset = sorted(articles, key=lambda id_asset: self.history[channel].get(id_asset, 0))[0]
+
+        self.history[channel][id_asset] = time.time()
+
+        article = Asset(id_asset)
+        return article
+
+
+
+
+
+    def get_free_asset(self, db=False):
+        if not db:
+            db=DB()
+
+        db.query("SELECT COUNT(id_object) from nx_assets WHERE id_folder=6")
+        count = db.fetchall()[0][0]
+        if count < self.max_articles:
+            return Asset(db=db)
+
+        db.query("SELECT id_object FROM nx_assets WHERE id_folder = 6 ORDER BY mtime ASC LIMIT 1")
+        return Asset(db.fetchall()[0][0], db=db)
+
+
 
     def on_main(self):
-        pass
+        from mod_google_headlines import google_headlines
+
+        db = DB()
+        for article in google_headlines():
+            db.query("SELECT id_object FROM nx_meta WHERE tag='identifier/guid' AND value = %s AND id_object IN (SELECT id_object FROM nx_meta WHERE tag='news_group' AND value=%s )", [article["identifier/guid"], article["news_group"] ])
+            if db.fetchall():
+                continue
+
+            db.query("SELECT id_object FROM nx_meta WHERE tag='title' AND value = %s AND id_object IN (SELECT id_object FROM nx_meta WHERE tag='news_group' AND value=%s )", [article["title"], article["news_group"] ])
+            if db.fetchall():
+                continue
+
+            asset = self.get_free_asset(db=db)
+            asset["id_folder"] = 6
+            asset["origin"] = "News"
+            asset["status"] = ONLINE
+            asset["qc/state"] = 4
+            asset.meta.update(article)
+            asset.save()
