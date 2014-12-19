ac8119cd9360b96d947ed02924241c5fcb9981fe
Martastain <martas@imm.cz>
2014-12-19 09:15:27 +0100

    Auth cleanup

--- services/hive/auth.py
+++ services/hive/auth.py
@@ -1,20 +1,6 @@
 from nx import *
 from nx.objects import *
 
-
-r = {
-    "can/mcr" : config["playout_channels"].keys(),
-    "can/rundown_edit" : config["playout_channels"].keys(),
-
-    "can/view" : range(0,10),
-    "can/asset_edit" : range(0,15),
-    "can/asset_create" : range(0,15),
-    
-    "can/action" : range(0,5),
-    "can/service_control" : range(0,20)
-    }
-
-# TODO: Use memcache for this
 class Sessions():
     def __init__(self):
         self.data = {}
@@ -26,12 +12,12 @@ class Sessions():
         db.query("SELECT id_user FROM nx_sessions WHERE key=%s", [key])
         try:
             user = User(db.fetchall()[0][0])
-            user.meta.update(r)
             return user
         except IndexError:
             return False
 
-
+    def __delitem__(self, key):
+        if key in self.data:
+            del(self.data[key])
 
 sessions = Sessions()
-
