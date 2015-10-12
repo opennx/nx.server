d15b9a1a8739b3ed33a6f85351beffcd43ce6ec8
Martin W <martas@imm.cz>
2015-10-12 14:03:58 +0200

    Update adduser.py

--- adduser.py
+++ adduser.py
@@ -5,7 +5,7 @@ from nx.objects import *
 print "\nCreate new nebula user\n"
 
 login = raw_input("Login: ").strip()
-password = raw_input("Login: ").strip()
+password = raw_input("Password: ").strip()
 is_admin = raw_input("Is it admin (yes/no): ").strip()
 
 u = User()
