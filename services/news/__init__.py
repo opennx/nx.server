62ffe1bc08d8145f832fb53eac0e68e106d87927
Martastain <martas@imm.cz>
2016-02-27 09:59:18 +0100

    News service refactoring (begin)

--- services/news/__init__.py
+++ services/news/__init__.py
@@ -1,8 +1,4 @@
-import urllib2
 import thread
-import uuid
-import email
-import re
 
 from datetime import datetime
 from BaseHTTPServer import BaseHTTPRequestHandler, HTTPServer
@@ -11,96 +7,7 @@ from nx import *
 from nx.services import BaseService
 from nx.objects import *
 
-from xml.etree import ElementTree as ET
-
-
-def rfc822(timestamp):
-    return  email.Utils.mktime_tz(email.Utils.parsedate_tz(timestamp))
-
-class NewsItem(dict):
-    pass
-
-def google_news_parser(item_data):
-    item = NewsItem()
-    try:
-        guid = item_data.find("guid").text.strip()
-        title = item_data.find("title").text.strip()
-    except:
-        return False
-
-    title_elms = title.strip().split("-")
-    title =  " ".join(title_elms[:-1]).strip()
-    source = title_elms[-1].strip()
-
-    if title.endswith("..."):
-       logging.warning("{} ends with tripple dot. skipping".format(title))
-       return False
-
-    pub_date = item_data.find("pubDate").text
-    pub_date = rfc822(pub_date.strip()) if pub_date else time.time()
-
-    #TODO: parse article
-#    desc1 = item_data.find("description").text.strip()
-#    matcher = r".*font\ size=\"\-1\"\&gt\;(?P<article>.*?)\&lt\;.*"
-#    m = re.match(matcher,desc1)
-#    desc2 = m.group("article")
-
- #   if desc2.endswith("<b>...</b>"):
- #       desc2 = ". ".join(desc2.split(". ")[:-1]).strip()+"."
-
-    item["title"]   = title
-    item["identifier/guid"] = guid
-    item["source"]  = source
-    #item["article"] = re.sub(r"(\| foto (.*))\.",".",strip_tags(desc2)).replace("|"," ")
-
-    return item
-
-
-
-
-class RSS():
-    def __init__(self, feed_url, parser=False):
-        self.feed_url = feed_url
-        self.parser = parser
-        data = urllib2.urlopen(self.feed_url).read()
-        self.feed = ET.XML(data).find("channel")
-
-    def default_parser(self, item_data):
-        item = NewsItem()
-        try:
-            guid = item_data.find("guid").text.strip()
-            title = item_data.find("title").text.strip()
-        except:
-            return False
-
-        description =  item_data.find("description").text
-        description = description.strip() if description else ""
-
-        pub_date = item_data.find("pubDate").text
-        pub_date = rfc822(pub_date.strip()) if pub_date else time.time()
-
-        item["identifier/guid"] = guid
-        item["title"] = title
-        item["article"] = description
-        item["description"] = ""
-        item["source"] = "" #TODO: Domain/RSS name here
-        return item
-
-    @property
-    def items(self):
-        for item_data in self.feed.findall("item"):
-            if self.parser:
-                item = self.parser(item_data)
-            else:
-                item = self.default_parser(item_data)
-            if item:
-                yield item
-
-
-
-
-
-
+from rss import *
 
 class ControlHandler(BaseHTTPRequestHandler):
     def log_request(self, code='-', size='-'):
@@ -160,12 +67,10 @@ class Service(BaseService):
         if not self.sources:
             self.sources = [
                     ["google_news", "rss", "https://news.google.com/news?cf=all&hl=cs&pz=1&ned=cs_cz&topic=h&output=rss", google_news_parser]
-                    #["dailysquat", "rss", "http://www.dailysquat.com/feed/", False],
-                    #["rfe", "rss", "http://www.rferl.org/api/c$tmnpuqdvki!ktqeo_qo", False]
                 ]
 
         self.history = {}
-        self.server = HTTPServer(('',port), ControlHandler)
+        self.server = HTTPServer(('', port), ControlHandler)
         self.server.service = self
         thread.start_new_thread(self.server.serve_forever,())
 
@@ -243,6 +148,4 @@ class Service(BaseService):
                 for item in feed.items:
                     item["news_group"] = group
                     self.push_item(item, db=db)
-
-
 
