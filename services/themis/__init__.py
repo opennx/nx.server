cb43e3ca0bdfcb686fa96716a46d7fbc25b4b659
Martastain <martas@imm.cz>
2016-03-05 09:24:22 +0100

    themis service prototype

---
+++ services/themis/__init__.py
@@ -0,0 +1,11 @@
+from nx import *
+from nx.services import BaseService
+
+from themis import Themis
+
+class Service(BaseService):
+    def on_init(self):
+        pass
+
+    def on_main(self):
+        pass
