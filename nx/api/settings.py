3f3ca4f14daef9ebb7cdbb7c00eba81df33ec3e7
Martastain <martas@imm.cz>
2016-10-28 11:23:24 +0200

    fixed meta_types

--- nx/api/settings.py
+++ nx/api/settings.py
@@ -1,9 +1,8 @@
 from nx import *
-from pprint import pprint
 
 def api_settings(**kwargs):
     return {'response' : 200, 'data' : config}
 
 def api_meta_types(**kwargs):
-    return {'response' : 200, 'data' : meta_types}
+    return {'response' : 200, 'data' : meta_types.dump}
 
